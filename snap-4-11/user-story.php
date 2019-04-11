36a9a06a4c23221b0c75e58199c04849c9216ac0
Jamie Amparan <jleespooks@gmail.com>
2019-04-11 09:08:14 -0600

    snap-4-11

---
+++ snap-4-11/user-story.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>user story</title>
+	</head>
+	<h1>User Story</h1>
+	<body>
+		<p>As a novice user I need to find an image url and copy.</p>
+	</body>
+</html>
