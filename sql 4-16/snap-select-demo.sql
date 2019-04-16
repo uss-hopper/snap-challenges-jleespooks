170c8984b52cf9bb25b6c8149d1652f036c40600
Jamie Amparan <jleespooks@gmail.com>
2019-04-16 15:07:02 -0600

    snap challenge 4-16 mysql. write along. examples

---
+++ sql 4-16/snap-select-demo.sql
@@ -0,0 +1,7 @@
+	SELECT
+	tweet.tweetContent,
+	profile.profileAtHandle,
+FROM tweet,
+		INNER JOIN `like` ON `like`.likeTweetId = tweet.tweetId,
+		INNER JOIN profile on profile.profile = `like`.likeProfileId
+	WHERE tweet.tweetId = HEX('0536faef082b454e9d444cdbe7887d7a');
