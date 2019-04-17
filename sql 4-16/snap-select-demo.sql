b9ed01c353c248b4b64b3257e19a4a51583caed4
Jamie Amparan <jleespooks@gmail.com>
2019-04-17 11:26:58 -0600

    snap challenge 4-16 mysql. write along/examples. also trying to untangle my nested projects.

--- sql 4-16/snap-select-demo.sql
+++ sql 4-16/snap-select-demo.sql
@@ -5,3 +5,4 @@ FROM tweet,
 		INNER JOIN `like` ON `like`.likeTweetId = tweet.tweetId,
 		INNER JOIN profile on profile.profile = `like`.likeProfileId
 	WHERE tweet.tweetId = HEX('0536faef082b454e9d444cdbe7887d7a');
+
