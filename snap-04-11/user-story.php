64da6522976e119359b921e77016cd6c927a9834
Jamie Amparan <jleespooks@gmail.com>
2019-04-17 16:07:06 -0600

    resolving git conflicts

---
+++ snap-04-11/user-story.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>user story</title>
+	</head>
+	<h1>User Story</h1>
+	<body>
+		<p>As a novice user I need to find an image url and copy.</p>
+		<a href="erd.php">erd</a>
+	</body>
+</html>
