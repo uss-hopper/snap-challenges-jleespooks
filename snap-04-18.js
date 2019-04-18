c78e16cb67122d02337d0b99f0197d65e516bb04
Jamie Amparan <jleespooks@gmail.com>
2019-04-18 09:06:26 -0600

    snap challenge. example pulled from web.

---
+++ snap-04-18.js
@@ -0,0 +1,16 @@
+
+
+()
+For i = 1 to 100
+print_number = True
+If i is divisible by 3 Then
+Print "Fizz"
+print_number = False
+End If
+If i is divisible by 5 Then
+Print "Buzz"
+print_number = False
+End If
+If print_number = True Then print i
+Print a newline
+Next i
