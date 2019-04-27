a0192026ee7d3f5f24b15fd044fce5fccd4fac22
Jamie Amparan <jleespooks@gmail.com>
2019-04-27 13:22:21 -0600

    snap challenge 04-27

---
+++ snap-04-25/oop.snap.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * writing a class with two state variable
+*
+ *
+**/
+
+public class facebook  {
+	//facebookAccount attributes
+private string = FacebookNumber;
+private string = facountNumber;
+private double balance;
+}
+
+//facebook methhods
+
+//the constructor
+public facebookaccount(string faccnumber) = faccnumber;
+accountName = accNumber;
+balanc 0;
